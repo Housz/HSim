457ab6cdba5107c4fe9f49b0efcf5bef3b4c8ddc
Housz <Housz@Housz.cn>
2023-11-23 14:54:52 +0800

    vec2

--- include/HSim/vec2.h
+++ include/HSim/vec2.h
@@ -2,10 +2,16 @@
 
 #include <iostream>
 #include <algorithm>
+#include <HSim/common.h>
 
 namespace HSim
 {
 
+/**
+ * @brief 2D vector
+ * 
+ * @tparam T : element type
+ */
 template <typename T>
 class Vec2
 {
@@ -13,6 +19,13 @@ class Vec2
         T x;
         T y;
 
+        template <typename T1>
+        friend std::ostream& operator<< ( std::ostream&, Vec2<T1>& );
+
+        /**
+         * @brief Construct a new Vec 2 object
+         * 
+         */
         Vec2() : x(0), y(0) {}
         Vec2(T x_, T y_) : x(x_), y(y_) {}
         Vec2(Vec2& v_) : x(v_.x), y(v_.y) {}
@@ -21,10 +34,7 @@ class Vec2
         void set(T x_, T y_) { x = x_; y = y_; }
         void set(Vec2& v_) { x = v_.x; y = v_.y; }
 
-        T length()
-        {
-            return std:;sqrt( x*x + y*y );
-        }
+        T length() { return std:;sqrt( x*x + y*y ); }
 
         void normalize() 
         {
@@ -39,32 +49,120 @@ class Vec2
             return Vec2<T> ( x/length, y/length );
         }
 
-        Vec2<T> add(T a_)
+        bool isEqual( Vec2<T> v_)
         {
-            return Vec2<T> ( x+a, y+a );
+            return (x == v_.x) && (y == v_.y);
         }
 
-        Vec2<T> add(Vec2& v_)
+        //// new_vec2 = this_vec2 (operator) parameters
+        
+        // add
+        Vec2<T> add( T a_ ) { return Vec2<T> ( x+a_, y+a_ ); }
+        Vec2<T> add( Vec2<T>& v_ ) { return Vec2<T> ( x+v_.x, y+v_.y ); }
+
+        // sub
+        Vec2<T> sub( T a_ ) { return Vec2<T> ( x-a_, y-a_ ); }
+        Vec2<T> sub( Vec2<T>& v_ ) { return Vec2<T> ( x-v_.x, y-v_.y ); }
+
+        // mul
+        Vec2<T> mul( T a_ ) { return Vec2<T> ( x*a_, y*a_ ); }
+        Vec2<T> mul( Vec2<T>& v_ ) { return Vec2<T> ( x*v_.x, y*v_.y ); }
+
+        // div
+        Vec2<T> div( T a_ ) { return Vec2<T> ( x/a_, y/a_ ); }
+        Vec2<T> div( Vec2<T>& v_ ) { return Vec2<T> ( x/v_.x, y/v_.y ); }
+
+        // dot
+        T dot( Vec2<T>& v_ ) { return x*v_.x + y*v_.y; }
+
+        // cross
+        T cross( Vec2<T>& v_ ) { return x*v_.y - v_.x*y; }
+        
+        
+        //// this_vec2 = this_vec2 (operator) parameters
+
+        void add_self(T a_ ) { x+=a_; y+=a_; }
+        void add_self( Vec2<T>& v_ ) { x+=v_.x, y+=v_.y; }
+
+        void sub_self( T a_ ) { x-=a_; y-=a_;  }
+        void sub_self( Vec2<T>& v_ ) { x-=v_.x; y-=v_.y; }
+
+        void mul_self( T a_ ) { x*=a_; y*=a_; }
+        void mul_self( Vec2<T>& v_ ) { x*=v_.x; y*=v_.y; }
+
+        void div_self( T a_ ) { x/=a_; y/=a_; }
+        void div_self( Vec2<T>& v_ ) { x/=v_.x; y/=v_.y; }
+
+
+        //// Operators
+
+        T& operator[](size_t i) 
         {
-            return Vec2<T> ( x+v_.x, y+v_.y );
+            assert(i < 2);
+            return (&x)[i];
         }
 
-        //sub
-        //mul
-        //div
+        Vec2& operator= ( Vec2<T>& v_ )
+        {
+            set(v_);
+            return (*this);
+        }
 
-        //<<
-        // template <typename T1>
-        // std::ostream& operator<< (std::ostream& os, Vec2<T1>& v_)
-        // {
-        //     os << "x: " << v_.x << " y: " << v_.y;
-        //     return os;
-        // }
+        Vec2 operator+ ( Vec2<T>& v_ ) { return add(v_); }
+
+        Vec2 operator- ( Vec2<T>& v_ ) { return sub(v_); }
+
+        Vec2 operator* ( Vec2<T>& v_ ) { return mul(v_); }
+        Vec2 operator* ( T a_ ) { return mul(a_); }
+
+        Vec2 operator/ ( Vec2<T>& v_ ) { return div(v_); }
+        Vec2 operator/ ( T a_ ) { return div(a_); }
+
+
+        Vec2& operator+= ( Vec2<T>& v_ )
+        {
+            add_self(v_);
+            return (*this);
+        }
+
+        Vec2& operator-= ( Vec2<T>& v_ )
+        {
+            sub_self(v_);
+            return (*this);
+        }
+
+        Vec2& operator*= ( Vec2<T>& v_ )
+        {
+            mul_self(v_);
+            return (*this);
+        }
+
+        Vec2& operator/= ( Vec2<T>& v_ )
+        {
+            div_self(v_);
+            return (*this);
+        }
+
+        bool operator== ( Vec2<T>& v_ ) { return isEqual(v_); }
+
+        bool operator!= ( Vec2<T>& v_ ) { return !isEqual(v_); }
+
 
     
 };
 
+typedef Vec2<int> vec2i;
 typedef Vec2<float> vec2f;
 typedef Vec2<double> vec2d;
+
+template <typename T1>
+std::ostream& operator<< (std::ostream& os, Vec2<T1>& v_)
+{
+    os << "x: " << v_.x << " y: " << v_.y << std::endl;
+    return os;
+}
+
+
+
 
 }
--- tests/test_vec2.cpp
+++ tests/test_vec2.cpp
@@ -1,15 +1,52 @@
 #include <iostream>
 #include <HSim/vec2.h>
 
+#define log(x) ( std::cout << x << std::endl )
+// #define log ( std::cout << )
+
 
 int main()
 {
     HSim::Vec2<double> v2(1, 2);
     HSim::vec2d vd(1, 2);
     HSim::vec2f vf(1, 2);
-    std::cout << v2.x << std::endl;
 
-    // std::cout << v2;
+    std::cout << v2 << vd << vf;
+
+    std::cout << v2.add(2);
+    auto vd1 = new HSim::vec2d(3, 4);
+    std::cout << *vd1 ;
+
+    std::cout << vd.add( 1.2 );
+    std::cout << vd.add( *(new HSim::vec2d(1, 2)) );
+    std::cout << vd.sub( 3.14 );
+    std::cout << vd.sub( *(new HSim::vec2d(1, 2)) );
+    std::cout << vd.mul( 2 );
+
+    vd.add_self( *(new HSim::vec2d(1, 2)) );
+    std::cout << vd;
+
+    std::cout << vd[1] << std::endl;
+
+    vd = *vd1;  std::cout << vd;
+    vd += *vd1; std::cout << vd;
+    vd -= *vd1; std::cout << vd;
+    vd *= *vd1; std::cout << vd;
+    vd /= *vd1; std::cout << vd;
+
+    std::cout << vd << *vd1;
+
+    std::cout << vd + *vd1;
+    std::cout << vd - *vd1 + vd;
+    std::cout << vd / *vd1 + vd;
+    std::cout << vd * 2 / 3;
+
+
+
+    auto is = vd == (*vd1);
+
+    
+    
 
     return 0;
 }
