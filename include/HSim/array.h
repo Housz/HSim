bcea994de47e295284ba46e9dd78b627f7f55302
Housz <Housz@Housz.cn>
2024-05-20 21:28:54 +0800

    update array

--- include/HSim/array.h
+++ include/HSim/array.h
@@ -6,8 +6,8 @@ namespace HSim
 {
     /**
      * @brief 2D/3D array base class
-     * 
-     * @tparam T 
+     *
+     * @tparam T
      */
     template <typename T>
     class Array
@@ -15,7 +15,7 @@ namespace HSim
     public:
         Array() {}
 
-        Array(const Array<T>& array_)
+        Array(const Array<T> &array_)
             : _data(array_._data)
         {
         }
@@ -23,14 +23,25 @@ namespace HSim
         ~Array() {}
 
     public:
-        void setData(std::vector<T> data) { _data = data; }
-        std::vector<T> getData() { return _data; }
+        void setData(const std::vector<T> &data) 
+        { 
+            _data = data;
+        }
 
-        void fill(T value) { std::fill(_data.begin(), _data.end(), value); }
+        std::vector<T> getData() 
+        { 
+            return _data;
+        }
+
+        void fill(T value) 
+        { 
+            std::fill(_data.begin(), _data.end(), value);
+        }
+
+        void 
 
     public:
         std::vector<T> _data;
-
     };
 
 } // namespace HSim
